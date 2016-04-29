086e36d31752c5a6af77ac13e42379ce2828af72
Darren Hicks <darren.hicks@gmail.com>
2016-04-29 14:38:15 -0600

    Adding route for puma_server_processes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ WebRedeploy::Engine.routes.draw do
   get 'fetch_origin' => "app#fetch_origin"
   get 'phased_restart' => "app#phased_restart"
   get 'pull_code' => "app#pull_code"
+  get 'puma_server_processes' => "app#puma_server_processes"
   get 'sanity_check' => "app#sanity_check"
   get 'switch_branch' => "app#switch_branch"
   get 'tail_log' => "app#tail_log"
